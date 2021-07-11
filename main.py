453a6f5dd763f672e5290fb315bc62e5c15f7a3e
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 12:06:59 +0100

    alphabets added and inputs added

---
+++ main.py
@@ -0,0 +1,5 @@
+
+# Asking for user inputs
+direction = input("Enter 'encode' to encrypt or 'decode' to decrypt: ")
+text = input("Enter the message: ").lower()
+shift = int(input("Enter the shift number:"))
