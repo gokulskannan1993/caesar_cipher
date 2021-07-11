d7fd72ce0cd6105cf0b697c4e8ced06b2bfd0aca
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 13:27:53 +0100

    all done

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from logic import caesar_cipher, goAgain
 from resources import clear, logo
 
 
+terminate = False
 
 
 # Asking for user input 
 clear()
 print(logo)
-operation = input("Enter 'encode' to encrypt or 'decode' to decrypt: ")
-text = input("Enter the message: ").lower()
-key = int(input("Enter the shift number:")) % 26
+
+while not terminate:
+    operation = input("Enter 'encode' to encrypt or 'decode' to decrypt: ")
+    text = input("Enter the message: ").lower()
+    key = int(input("Enter the shift number:")) % 26
 
 
-# Switch operations according to User input 
-caesar_cipher(operation, text, key)
+    # Switch operations according to User input 
+    caesar_cipher(operation, text, key)
 
+    # If the user wants to repeat
+    again = input("Enter 'yes' to do again or 'no' otherwise: ")
+    if again == "no":
+        terminate = True
+        print("All Done, Program is Terminated")
