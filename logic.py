81341836ec47b8ffffe929c498db3ec220b59d8b
Gokul Kannan <45422557+gokulskannan1993@users.noreply.github.com>
2021-07-11 14:40:24 +0100

    all done

--- logic.py
+++ logic.py
@@ -19,11 +19,3 @@ def caesar_cipher(operation, text, key):
             answer += character
     print(f"The {operation}d text is '{answer}'")
 
-
-# Ask to do again
-def goAgain():
-    again = input("Do you want to do another operation? Press 'Yes' to proceed or Anything else to Terminate the app").upper()
-    if(again == "YES"):
-        operation = input("Enter 'encode' to encrypt or 'decode' to decrypt or 'exit' to terminate: ")
-    else:
-        terminate = True
